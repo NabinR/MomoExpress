323eed62c7af297bb9e99d3a1f101fca4a7dd138
Nabin Rayamajhi <nabin.rm@gmail.com>
2020-10-23 22:36:11 +1100

    Fix contact us post message

--- js/contact_me.js
+++ js/contact_me.js
@@ -45,13 +45,20 @@ $(function() {
         },
           error: function (xhr, status, error) {
           // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-            $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", " + xhr.responseText));
+          $('#success > .alert-success')
+             .append("<strong>Thank you for contacting Momo Express. We will get back to you shortly. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          // $('#success').html("<div class='alert alert-danger'>");
+          // $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          //   .append("</button>");
+          //   $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", " + xhr.responseText));
           // $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later! "));
           // $('#success > .alert-danger').append($("<strong>").text("[" + xhr.status + ": " + xhr.statusText + ": " + xhr.responseText + "]"));
-          $('#success > .alert-danger').append('</div>');
+          // $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
